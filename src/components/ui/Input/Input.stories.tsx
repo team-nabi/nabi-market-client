7d7ac5844bade55a8412ea1bff2859a52b881130
oaoong <pizzachicken@kakao.com>
2023-10-28 00:56:34 +0900

    :tada: Input 컴포넌트 생성

---
+++ src/components/ui/Input/Input.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Input } from './Input'
+
+const meta = {
+  title: 'UI/Input',
+  component: Input,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Input>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Normal: Story = {
+  args: {},
+}
+
+export const Disabled: Story = {
+  args: {},
+}
