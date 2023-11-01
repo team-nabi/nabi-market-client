699f8f3c195e8a9d8d0489ac1ccf8530dec6566c
juyeon-park <lpjy1107@gmail.com>
2023-11-01 10:38:10 +0900

    :tada: 이미지 슬라이더 구현

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -1,9 +1,14 @@
 import AlarmIcon from '/public/images/bell.svg'
 import MenuIcon from '/public/images/menu-icon.svg'
+import LeftIcon from '/public/images/icon-left.svg'
+import RightIcon from '/public/images/icon-right.svg'
+
 
 const Assets = {
   menuIcon: MenuIcon,
   alarmIcon: AlarmIcon,
+  leftIcon: LeftIcon,
+  rightIcon: RightIcon
 } as const
 
 export default Assets
