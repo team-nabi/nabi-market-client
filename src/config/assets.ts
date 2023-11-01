80ed5d6f8a58605acc08132785f3393a72765e3a
oaoong <pizzachicken@kakao.com>
2023-11-01 16:40:26 +0900

    :bug: merge conlict 처리

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -1,17 +1,17 @@
 import AlarmIcon from '/public/images/bell.svg'
 import GoogleIcon from '/public/images/google.png'
+import LeftIcon from '/public/images/icon-left.svg'
+import RightIcon from '/public/images/icon-right.svg'
 import KakaoIcon from '/public/images/kakao.png'
 import Logo from '/public/images/logo.svg'
 import MenuIcon from '/public/images/menu-icon.svg'
-import LeftIcon from '/public/images/icon-left.svg'
-import RightIcon from '/public/images/icon-right.svg'
 import XIcon from '/public/images/x-icon.svg'
 
 const Assets = {
   menuIcon: MenuIcon,
   alarmIcon: AlarmIcon,
   leftIcon: LeftIcon,
-  rightIcon: RightIcon
+  rightIcon: RightIcon,
   googleIcon: GoogleIcon,
   kakaoIcon: KakaoIcon,
   logo: Logo,
