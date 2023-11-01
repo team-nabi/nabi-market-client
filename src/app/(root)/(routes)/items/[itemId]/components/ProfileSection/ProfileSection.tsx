4cc6ba2557fcb86ddee6df55a72313f189fddea2
juyeon-park <lpjy1107@gmail.com>
2023-11-02 00:45:22 +0900

    :tada: 코드리뷰 반영 및 아바타 디폴트 이미지 설정 완료

--- src/app/(root)/(routes)/items/[itemId]/components/ProfileSection/ProfileSection.tsx
+++ src/app/(root)/(routes)/items/[itemId]/components/ProfileSection/ProfileSection.tsx
@@ -2,18 +2,15 @@ import { Avatar, AvatarImage } from '@/components/ui/Avatar'
 import { TYPHOGRAPHY } from '@/styles/sizes'
 
 type ProfileSectionProps = {
-  profileImg?: string
+  profileImg: string | null
   userName: string
 }
 
-const ProfileSection = ({
-  profileImg = 'https://github.com/shadcn.png',
-  userName,
-}: ProfileSectionProps) => {
+const ProfileSection = ({ profileImg, userName }: ProfileSectionProps) => {
   return (
     <section className="flex w-full py-2 border-b-[1px] items-center">
       <Avatar size="md">
-        <AvatarImage src={profileImg} />
+        <AvatarImage imgUrl={profileImg} />
       </Avatar>
       <div className={`ml-9 ${TYPHOGRAPHY.profile}`}>{userName}</div>
     </section>
--- src/app/(root)/(routes)/items/[itemId]/page.tsx
+++ src/app/(root)/(routes)/items/[itemId]/page.tsx
@@ -10,7 +10,7 @@ const ItemPage = ({ params }: ItemPageProps) => {
   return (
     <main className="flex-col min-h-screen  bg-background-color">
       <div>이미지 슬라이더 영역</div>
-      <ProfileSection userName="임시이름" />
+      <ProfileSection profileImg={null} userName="임시이름" />
       <div>아이템 상세정보 영역</div>
     </main>
   )
