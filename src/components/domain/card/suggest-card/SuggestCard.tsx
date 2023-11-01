d443ffe54462be412f5a1e324e2765dda4a8c1c3
juyeon-park <lpjy1107@gmail.com>
2023-11-01 19:47:04 +0900

    :tada: 제안 카드 컴포넌트 구현

--- src/components/domain/card/suggest-card/SuggestCard.tsx
+++ src/components/domain/card/suggest-card/SuggestCard.tsx
@@ -1,20 +1,48 @@
+import Button from '@/components/ui/Button'
 import Card from '@/components/ui/Card'
-import { CardFlex, CardImage } from '@/components/ui/Card/Card'
+import { CardFlex, CardImage, CardText } from '@/components/ui/Card/Card'
 
-const SuggestCard = () => {
+type SuggestCardProps = {
+  thumbNail?: string
+  cardTitle: string
+  itemName: string
+  priceRange: string
+  suggestionType: string
+}
+
+const SuggestCard = ({
+  thumbNail = 'https://cdn.cetizen.com/CDN/market/market_large_crop/202203/20220318/220318152808_1_2913635.jpg',
+  cardTitle,
+  itemName,
+  priceRange,
+  suggestionType,
+}: SuggestCardProps) => {
   return (
     <Card size="md">
-      <CardFlex>
+      <CardFlex
+        direction={'row'}
+        justify={'start'}
+        align={'center'}
+        gap={'space'}
+      >
         <div className="h-full w-36 relative">
           <CardImage
-            src={
-              'https://cdn.cetizen.com/CDN/market/market_large_crop/202203/20220318/220318152808_1_2913635.jpg'
-            }
-            alt="Picture of the author"
+            src={thumbNail}
+            alt="thumbNail"
             layout="fill"
             objectFit="cover"
           />
         </div>
+        <CardFlex direction={'col'} justify={'between'} className="h-full grow">
+          <CardText type={'title'}>{cardTitle}</CardText>
+          <CardText type={'description'}>{itemName}</CardText>
+          <CardText type={'description'}>{priceRange}</CardText>
+          <div className="flex justify-end">
+            <Button variant={'gradation'} size={'sm'}>
+              {suggestionType === 'poke' ? '찔러보기' : '오퍼하기'}
+            </Button>
+          </div>
+        </CardFlex>
       </CardFlex>
     </Card>
   )
