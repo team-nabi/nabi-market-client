8a501427ec5bc3b37316a9cf89f1001f71331068
oaoong <pizzachicken@kakao.com>
2023-11-02 17:34:27 +0900

    :bug: msw ssr에서 동작하도록 수정

--- src/app/(root)/(routes)/(home)/components/TestBlock.tsx
+++ src/app/(root)/(routes)/(home)/components/TestBlock.tsx
@@ -1,25 +1,24 @@
-'use client'
-
+// 'use client'
 import React, { useEffect } from 'react'
 import { getTest } from '@/services/test/test'
 
-// async function getTestValue() {
-//   const res = await getTest()
-//   const data = await res.json()
-//   return data
-// }
+async function getTestValue() {
+  const res = await getTest()
+  const data = await res.json()
+  return data
+}
 
-export default function TestBlock() {
-  // const data = await getTestValue()
-  // console.log(data.message)
+export default async function TestBlock() {
+  const data = await getTestValue()
+  console.log(data.message)
 
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getTest()
-      console.log(await data.json())
-    }
-    fetchData()
-  }, [])
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const data = await getTest()
+  //     console.log(await data.json())
+  //   }
+  //   fetchData()
+  // }, [])
 
-  return <div>{'index '}</div>
+  return <div>{'index ' + data.message}</div>
 }
