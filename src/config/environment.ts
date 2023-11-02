dfab0481b8b30d23ba50083918fb7db7d2e5e81e
oaoong <pizzachicken@kakao.com>
2023-11-03 01:17:20 +0900

    :tada: validate 함수 작성

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,3 +1,5 @@
+import { assertValue } from '@/utils'
+
 export const Environment = {
   apiAddress: () =>
     process.env.NEXT_PUBLIC_API_MOCKING === 'enabled'
@@ -5,4 +7,9 @@ export const Environment = {
       : process.env.NEXT_PUBLIC_API_ADDRESS,
   apiMocking: () =>
     process.env.NEXT_PUBLIC_API_MOCKING === 'enabled' ? 'enabled' : 'disabled',
+  tokenName: () => assertEnv('NEXT_PUBLIC_API_TOKEN_NAME'),
+}
+
+function assertEnv(key: string) {
+  return assertValue<string>(process.env[key], `${key}가 설정되지 않았습니다.`)
 }
--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -1,4 +1,6 @@
+import Cookies from 'js-cookie'
 import ApiEndPoint from '@/config/apiEndPoint'
+import { Environment } from '@/config/environment'
 import apiClient from '../apiClient'
 
 const getKakaoLogin = async () => {
@@ -13,4 +15,16 @@ const getGoogleLogin = async () => {
   return response
 }
 
-export { getKakaoLogin, getGoogleLogin }
+const getValidateUser = async () => {
+  const token = Cookies.get(Environment.tokenName())
+  const response = await apiClient.get(
+    ApiEndPoint.getValidateUser(),
+    {},
+    {
+      Authorization: `Bearer ${token}`,
+    },
+  )
+  return response
+}
+
+export { getKakaoLogin, getGoogleLogin, getValidateUser }
