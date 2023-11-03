3c90909c75033d11ed40889395e034dfff154db7
juyeon-park <lpjy1107@gmail.com>
2023-11-03 15:18:13 +0900

    :truck: itemHandler 통합

--- src/lib/msw/mocks/handlers.ts
+++ src/lib/msw/mocks/handlers.ts
@@ -1,6 +1,5 @@
 import authHandlers from './authHandlers'
-import { itemHandlers } from './itemHandler'
-import { testHandlers } from './testHandler'
 import { itemHandlers } from './itemHandlers'
+import { testHandlers } from './testHandler'
 
 export const handlers = [...testHandlers, ...authHandlers, ...itemHandlers]
