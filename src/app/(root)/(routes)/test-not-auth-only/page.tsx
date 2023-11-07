0176036f2532667d5e90e04199c29a638ee2080c
oaoong <pizzachicken@kakao.com>
2023-11-08 01:19:14 +0900

    :tada: 에러 처리

--- src/app/(root)/(routes)/test-not-auth-only/page.tsx
+++ src/app/(root)/(routes)/test-not-auth-only/page.tsx
@@ -1,7 +1,40 @@
+'use client'
+
 import React from 'react'
+import { useRouter } from 'next/navigation'
+import { useToast } from '@/components/ui/Toast/useToast'
+import { handleApiError } from '@/lib/handleApiError'
+import { getTest } from '@/services/test/test'
 
 const NotAuthOnly = () => {
-  return <div>NotAuthOnly</div>
+  const router = useRouter()
+  const { toast } = useToast()
+
+  const onClickButton = async () => {
+    try {
+      await getTest()
+    } catch (error) {
+      const { shouldRedirect, message } = handleApiError(error)
+      if (shouldRedirect) {
+        router.push(shouldRedirect)
+      } else {
+        console.log(shouldRedirect, error)
+        toast({
+          title: 'Error',
+          description: message,
+          duration: 1000,
+        })
+      }
+    }
+  }
+
+  return (
+    <div>
+      NotAuthOnly
+      <br />
+      <button onClick={onClickButton}>login</button>
+    </div>
+  )
 }
 
 export default NotAuthOnly
---
+++ src/components/ui/Toast/index.tsx
@@ -0,0 +1,27 @@
+import {
+  type ToastProps,
+  type ToastActionElement,
+  ToastProvider,
+  ToastViewport,
+  Toast,
+  ToastTitle,
+  ToastDescription,
+  ToastClose,
+  ToastAction,
+} from './Toast'
+import { Toaster } from './Toaster'
+import { useToast } from './useToast'
+
+export type { ToastProps, ToastActionElement }
+
+export {
+  ToastProvider,
+  ToastViewport,
+  Toast,
+  ToastTitle,
+  ToastDescription,
+  ToastClose,
+  ToastAction,
+  Toaster,
+  useToast,
+}
