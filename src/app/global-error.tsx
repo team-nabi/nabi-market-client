ca65c74356f7cb5a4932ec89d005d392b15f38dc
oaoong <pizzachicken@kakao.com>
2023-11-08 01:42:02 +0900

    :bug: 빌드 에러 처리

---
+++ src/app/global-error.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+export default function GlobalError({
+  _error,
+  reset,
+}: {
+  _error: Error
+  reset: () => void
+}) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from 'next'
 import Header from '@/components/domain/Header'
 import { Toaster } from '@/components/ui/Toast/Toaster'
+import { Environment } from '@/config/environment'
 import AuthProvider from '@/contexts/AuthProvider'
 import MSWWrapper from '@/contexts/MSWWrapper'
 import TanstackQueryContext from '@/contexts/TanstackQueryContext'
 import ThemeProviderContext from '@/contexts/ThemeProviderContext'
+import { initMockApi } from '@/lib/msw/initMockApi'
 import '@/styles/globals.css'
 
 export const metadata: Metadata = {
   title: '나비장터',
   description: '물물교환 플랫폼 나비장터입니다.',
+}
+
+if (Environment.apiMocking() === 'enabled') {
+  console.log('Mocking enabled')
+  initMockApi()
 }
 
 export default async function RootLayout({
