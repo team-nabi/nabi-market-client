aa8fe2e1658fd5e7419cbff418f42af15207cec3
juyeon-park <lpjy1107@gmail.com>
2023-11-11 23:56:25 +0900

    :sparkles: 코드리뷰 반영

--- src/app/(root)/(routes)/(home)/page.tsx
+++ src/app/(root)/(routes)/(home)/page.tsx
@@ -1,18 +1,14 @@
 import Link from 'next/link'
-import Slider from '@/components/domain/Slider/Slider'
 import { DarkModeButton } from '@/components/ui/DarkModeButton'
-import Assets from '@/config/assets'
+import HomeSlider from './components/HomSlider'
 import TestBlock from './components/TestBlock'
 
 function HomePage() {
-  const bannerArr = [
-    { _id: 1, image: Assets.banner1 },
-    { _id: 2, image: Assets.banner2 },
-    { _id: 3, image: Assets.banner3 },
-  ]
   return (
     <main className="flex flex-col items-center justify-between min-h-screen  text-4xl font-bold text-text-color bg-background-color">
-      <Slider imageData={bannerArr} imageAspectRatio="auto" />
+      <section>
+        <HomeSlider />
+      </section>
       <Link href={'/test-auth-only'}>auth only</Link>
       <Link href={'/test-not-auth-only'}>not auth only</Link>
       <TestBlock />
