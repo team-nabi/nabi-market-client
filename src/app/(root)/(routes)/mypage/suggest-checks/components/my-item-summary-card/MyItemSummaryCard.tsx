484892cff97fd93944cc9d4b6af306f686b8323b
doggopawer <doggopawer@gmail.com>
2023-11-12 23:17:31 +0900

    :construction: 서버 컴포넌트 관련 오류로 인한 작업 진행중

--- src/app/(root)/(routes)/mypage/suggest-checks/components/my-item-summary-card/MyItemSummaryCard.tsx
+++ src/app/(root)/(routes)/mypage/suggest-checks/components/my-item-summary-card/MyItemSummaryCard.tsx
@@ -1,25 +1,12 @@
-import { formatDistanceToNow } from 'date-fns'
-import koLocale from 'date-fns/locale/ko'
-import TradeStateCard from '@/components/domain/card/trade-state-card'
-import { getItemInfo } from '@/services/item/item'
+import TradeStatusCard from '@/components/domain/card/trade-status-card'
+import { Item } from '@/types'
 
-async function getItemValue(itemId: string) {
-  try {
-    const res = await getItemInfo(itemId)
-    const data = await res
-    console.log(res)
-    data.data.cardResponseDto.image =
-      'https://cdn.cetizen.com/CDN/market/market_large_crop/202203/20220318/220318152808_1_2913635.jpg'
-    console.log(data)
-    return data.data.cardResponseDto
-  } catch (e) {
-    console.log(e)
-  }
+type MyItemSummaryCardProps = {
+  item: Item
+  params: any
 }
 
-const MyItemSummaryCard = async ({ params }: { params: any }) => {
-  const data = await getItemValue('3')
-  console.log('ditto', data)
-  return <TradeStateCard item={data} className={'border-none'} />
+const MyItemSummaryCard = ({ item, params }: MyItemSummaryCardProps) => {
+  return <TradeStatusCard item={item} className={'border-none'} />
 }
 export default MyItemSummaryCard
--- src/app/(root)/(routes)/mypage/suggest-checks/page.tsx
+++ src/app/(root)/(routes)/mypage/suggest-checks/page.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from 'react'
 import PageTitle from '@/components/domain/page-title'
+import { getItemInfo } from '@/services/item/item'
 import MyItemSummaryCard from './components/my-item-summary-card'
 import SuggestCheckList from './components/suggest-check-list'
 
@@ -9,13 +10,29 @@ interface SuggestCheckListPageProps {
   }
 }
 
-const SuggestCheckListPage: FunctionComponent<SuggestCheckListPageProps> = ({
-  params,
-}: SuggestCheckListPageProps) => {
+async function getItemValue(itemId: string) {
+  try {
+    const res = await getItemInfo(Number(itemId))
+    const data = await res
+    data.data.cardResponseDto.image =
+      'https://cdn.cetizen.com/CDN/market/market_large_crop/202203/20220318/220318152808_1_2913635.jpg'
+    data.data.cardResponseDto.createdAt = '2023-11-01T08:08:00'
+    return data.data.cardResponseDto
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const SuggestCheckListPage: FunctionComponent<
+  SuggestCheckListPageProps
+> = async ({ params }: SuggestCheckListPageProps) => {
+  const data = await getItemValue('3')
+  console.log(data)
   return (
     <div>
-      <PageTitle title="물건 목록" />
-      <MyItemSummaryCard params={params} />
+      <PageTitle title="제안 확인" />
+      {/* <MyItemSummaryCard item={data} params={params} /> */}
+      {JSON.stringify(data)}
       <SuggestCheckList />
     </div>
   )
