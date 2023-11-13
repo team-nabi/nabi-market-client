556056b2159dc5dca5a9fde6690e44b7614f838b
juyeon-park <lpjy1107@gmail.com>
2023-11-13 15:05:51 +0900

    :sparkles: 버그 해결

--- src/config/apiEndPoint.ts
+++ src/config/apiEndPoint.ts
@@ -7,7 +7,7 @@ const ApiEndPoint = {
   items: (cursorId: number) => `/items?cursorId=${cursorId}`,
   myItems: (cursorId: number) => `/my-items?cursorId=${cursorId}`,
   dibs: (itemId: number) => `/dib/${itemId}`,
-  suggestions: (itemId: string) => `/${itemId}/available-cards`,
+  suggestions: (itemId: number) => `/${itemId}/available-cards`,
   suggestChecks: (cursorId: number) => `/suggest-checks?cursorId=${cursorId}`,
   putUserProfile: () => '/users/profile-image',
   putUserNickname: () => '/users/nickname',
