fd8582aaee4ddf5a379d70ac2f9b1f1684ce0949
juyeon-park <lpjy1107@gmail.com>
2023-11-13 23:07:36 +0900

    :bug: 에러 해결

--- src/components/domain/buttons/login-buttons/LoginButtons.tsx
+++ src/components/domain/buttons/login-buttons/LoginButtons.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import Button from '@/components/ui/Button'
+import Button from '@/components/ui/button'
 import Assets from '@/config/assets'
 
 const KakaoLoginButton = ({ onClickButton }: { onClickButton: () => void }) => {
