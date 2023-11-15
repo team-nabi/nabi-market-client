efd6b0294c25a7aff261532c24758d9a2619eda9
juyeon-park <lpjy1107@gmail.com>
2023-11-16 02:19:34 +0900

    :tada: 메인페이지 카테고리영역 구현

--- src/app/(root)/(routes)/(home)/page.tsx
+++ src/app/(root)/(routes)/(home)/page.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link'
 import { DarkModeButton } from '@/components/ui/dark-mode-button'
-import HomeSlider from './components/HomSlider'
+import BannerSection from './components/BannerSection'
+import CategorySection from './components/CategorySection'
 import TestBlock from './components/TestBlock'
 
 function HomePage() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen  text-4xl font-bold text-text-color bg-background-color">
-      <section>
-        <HomeSlider />
-      </section>
+    <main className="flex flex-col items-center min-h-screen  text-4xl font-bold text-text-color bg-background-color gap-8">
+      <BannerSection />
+      <CategorySection />
       <Link href={'/test-auth-only'}>auth only</Link>
       <Link href={'/test-not-auth-only'}>not auth only</Link>
       <TestBlock />
