539984d33e965f45b8d4c88a52a17539dd182670
juyeon-park <lpjy1107@gmail.com>
2023-11-16 14:15:59 +0900

    :construction: 인기 카드 영역 틀 잡기

---
+++ src/app/(root)/(routes)/(home)/components/PopularCardSection.tsx
@@ -0,0 +1,5 @@
+const PopularCardSection = () => {
+  return <div>인기 카드 영역</div>
+}
+
+export default PopularCardSection
--- src/app/(root)/(routes)/(home)/page.tsx
+++ src/app/(root)/(routes)/(home)/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { DarkModeButton } from '@/components/ui/dark-mode-button'
 import BannerSection from './components/BannerSection'
 import CategorySection from './components/CategorySection'
+import PopularCardSection from './components/PopularCardSection'
 import TestBlock from './components/TestBlock'
 
 function HomePage() {
@@ -9,6 +10,7 @@ function HomePage() {
     <main className="flex flex-col items-center min-h-screen  text-4xl font-bold text-text-color bg-background-color gap-8">
       <BannerSection />
       <CategorySection />
+      <PopularCardSection />
       <Link href={'/test-auth-only'}>auth only</Link>
       <Link href={'/test-not-auth-only'}>not auth only</Link>
       <TestBlock />
