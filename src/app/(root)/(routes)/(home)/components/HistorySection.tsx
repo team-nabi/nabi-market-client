991bdbe1fb64416f045b08b3d97c6ddce598b4cf
juyeon-park <lpjy1107@gmail.com>
2023-11-16 18:33:10 +0900

    :tada: 메인페이지 거래성사영역 최종 구현

--- src/app/(root)/(routes)/(home)/components/HistorySection.tsx
+++ src/app/(root)/(routes)/(home)/components/HistorySection.tsx
@@ -2,39 +2,20 @@
 
 import HistoryCard from '@/components/domain/card/trade-history-card'
 import useRecentHistoryQuery from '@/hooks/api/queries/useRecentHistoryQuery'
+import { TradeHistory } from '@/types/tradeHistory'
 
-const dummyData = [
-  {
-    historyId: '1',
-    fromCard: {
-      cardId: 9,
-      itemName: '아이폰12',
-      thumbnail:
-        'https://cdn.cetizen.com/CDN/market/market_large_crop/202203/20220318/220318152808_1_2913635.jpg',
-      priceRange: 'PRICE_RANGE_ONE',
-    },
-    toCard: {
-      cardId: 10,
-      itemName: '아이폰11',
-      thumbnail:
-        'https://cdn.cetizen.com/CDN/market/market_large_crop/202203/20220318/220318152808_1_2913635.jpg',
-      priceRange: 'PRICE_RANGE_ONE',
-    },
-    createdAt: '2023-11-10T14:42:09',
-    modifiedAt: '2023-11-10T14:42:34',
-  },
-]
 const HistorySection = () => {
   const { data: historyData } = useRecentHistoryQuery()
-  console.log(historyData)
-
+  const historyList = historyData.data.historyList
   return (
-    <section className="flex flex-col w-full px-4 gap-6">
-      <div className="text-[24px] font-bold">최근 거래성사된 물건들</div>
-      {dummyData.map((data) => (
-        <HistoryCard key={data.historyId} history={data} />
-      ))}
-    </section>
+    historyList.length !== 0 && (
+      <section className="flex flex-col w-full px-4 gap-6">
+        <div className="text-[24px] font-bold">최근 거래성사된 물건들</div>
+        {historyList.map((data: TradeHistory) => (
+          <HistoryCard key={data.historyId} history={data} />
+        ))}
+      </section>
+    )
   )
 }
 
