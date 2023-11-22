5734725e5feb3a2e89e4a5ca102185cf56942eda
juyeon-park <lpjy1107@gmail.com>
2023-11-22 13:02:32 +0900

    :sparkles:에러처리 추가

--- src/app/(root)/(routes)/(home)/components/PopularCardSection.tsx
+++ src/app/(root)/(routes)/(home)/components/PopularCardSection.tsx
@@ -1,14 +1,28 @@
 import { getPopularCardList } from '@/services/card/card'
 import PopularCardSlider from './Slider'
 
+async function getPopularCardValue() {
+  try {
+    const res = await getPopularCardList()
+    const data = await res
+    return data.data
+  } catch (error: any) {
+    console.log(error)
+    throw new Error(error.message)
+  }
+}
+
 const PopularCardSection = async () => {
-  const data = await getPopularCardList()
+  //const data = await getPopularCardList()
+  const data = await getPopularCardValue()
 
   return (
-    <section className="flex flex-col w-full  gap-6">
-      <div className="text-[24px] font-bold px-4">오늘의 인기 물건</div>
-      <PopularCardSlider cardData={data.data} />
-    </section>
+    data && (
+      <section className="flex flex-col w-full  gap-6">
+        <div className="text-[24px] font-bold px-4">오늘의 인기 물건</div>
+        <PopularCardSlider cardData={data} />
+      </section>
+    )
   )
 }
 
