d1bcd5227c67f8b399dd8d6ce6b7332e5c29441a
doggopawer <doggopawer@gmail.com>
2023-11-27 19:13:48 +0900

    :tada: 제안 카드 낙관적 업데이트 기능 구현

--- src/hooks/api/mutations/useMySuggestionUpdateMutation.ts
+++ src/hooks/api/mutations/useMySuggestionUpdateMutation.ts
@@ -1,5 +1,10 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
 import {
+  InfiniteData,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query'
+import {
+  GetMySuggestionListRes,
   PutMySuggestionStatusReq,
   putMySuggestionStatus,
 } from '@/services/suggestion/suggestion'
@@ -19,29 +24,45 @@ export const useMySuggestionUpdateMutation = (
     }: PutMySuggestionStatusReq) => {
       await putMySuggestionStatus({ fromCardId, toCardId, isAccepted })
     },
-    onMutate: async (variables: any) => {
-      console.log('onMutate', variables)
-      //   const oldMySuggestionList = queryClient.getQueryData([
-      //     'my-suggestions',
-      //     suggestionType,
-      //     directionType,
-      //     myCardId,
-      //   ])
-      //   console.log(oldMySuggestionList)
-      // },
-      // onError: (error, variable, rollback) => {
-      //   if (rollback) rollback()
-      //   else console.log(error)
-      // },
-      // onSettled: () => {
-      //   queryClient.invalidateQueries([
-      //     'my-suggestions',
-      //     suggestionType,
-      //     directionType,
-      //     cardId,
-      //   ])
-      // },
-      // TODO : 에러처리 및 각 종 Optimistic Update 관련 기능 처리하기
+    onMutate: async ({ fromCardId, isAccepted }: PutMySuggestionStatusReq) => {
+      await queryClient.cancelQueries({
+        queryKey: ['my-suggestions', suggestionType, directionType, cardId],
+      })
+      const oldMySuggestionList:
+        | InfiniteData<GetMySuggestionListRes, unknown>
+        | undefined = queryClient.getQueryData([
+        'my-suggestions',
+        suggestionType,
+        directionType,
+        cardId,
+      ])
+
+      const newMySuggestionList = {
+        ...oldMySuggestionList,
+        pages: oldMySuggestionList?.pages.map((page) => ({
+          ...page,
+          data: {
+            ...page.data,
+            suggestionList: page.data.suggestionList.map((suggestion) =>
+              suggestion.cardInfo.cardId === fromCardId
+                ? {
+                    ...suggestion,
+                    suggestionInfo: {
+                      ...suggestion.suggestionInfo,
+                      suggestionStatus: isAccepted ? 'ACCEPTED' : 'REFUSED',
+                    },
+                  }
+                : suggestion,
+            ),
+          },
+        })),
+      }
+      queryClient.setQueryData(
+        ['my-suggestions', suggestionType, directionType, cardId],
+        newMySuggestionList,
+      )
+
+      return { oldMySuggestionList }
     },
 
     onSettled: () => {
@@ -49,5 +70,11 @@ export const useMySuggestionUpdateMutation = (
         queryKey: ['my-suggestions', suggestionType, directionType, cardId],
       })
     },
+    onError: (error, variable, rollback) => {
+      console.log('에러', error)
+      // if (rollback) rollback()
+      // else console.log(error)
+    },
+    // TODO : 에러처리 및 각 종 Optimistic Update 관련 기능 처리하기
   })
 }
