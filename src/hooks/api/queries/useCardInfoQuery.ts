af2266e5c2d067203500bf6634f2233bec96c7e0
juyeon-park <lpjy1107@gmail.com>
2023-11-27 16:00:23 +0900

    :sparkles: staleTime 변경

--- src/hooks/api/queries/useCardInfoQuery.ts
+++ src/hooks/api/queries/useCardInfoQuery.ts
@@ -8,12 +8,14 @@ const useCardInfoQuery = (cardId: number, isLoggedIn: boolean) => {
   const token = Cookies.get(Environment.tokenName())
 
   return useQuery({
-    queryKey: [cardId, 'cardInfo', token, isLoggedIn] as const,
+    // eslint-disable-next-line @tanstack/query/exhaustive-deps
+    queryKey: [cardId, 'cardInfo'] as const,
     queryFn: () => {
       if (isLoggedIn) apiClient.setDefaultHeader('Authorization', `${token}`)
       const res = getCardInfo(cardId)
       return res
     },
+    staleTime: 0,
   })
 }
 
