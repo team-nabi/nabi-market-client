f705d6fdb3843475a6729fe801e8ddb9786d910d
oaoong <pizzachicken@kakao.com>
2023-11-29 16:39:54 +0900

    :sparkles: 불필요한 리턴 정리

--- src/hooks/api/queries/useNotificationCountQuery.ts
+++ src/hooks/api/queries/useNotificationCountQuery.ts
@@ -4,10 +4,7 @@ import { getNotificationCount } from '@/services/notification/notification'
 const useNotificationCountQuery = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
   return useQuery({
     queryKey: ['notificationCount'] as const,
-    queryFn: () => {
-      const res = getNotificationCount()
-      return res
-    },
+    queryFn: () => getNotificationCount(),
     refetchInterval: 1000 * 60 * 3,
     enabled: isLoggedIn,
   })
