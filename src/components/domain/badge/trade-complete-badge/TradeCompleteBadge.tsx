60d40bcb347119bca136d3dbcd4f43f19624e840
juyeon-park <lpjy1107@gmail.com>
2023-12-02 16:27:06 +0900

    :bug: 거래성사된 물건 상세정보페이지 조회 안되는 버그 해결 (#148)
    Co-authored-by: 정재희 <pizzachicken@kakao.com>

--- src/components/domain/badge/trade-complete-badge/TradeCompleteBadge.tsx
+++ src/components/domain/badge/trade-complete-badge/TradeCompleteBadge.tsx
@@ -2,7 +2,7 @@ import Badge from '@/components/ui/badge'
 
 const TradeCompleteBadge = () => (
   <Badge variant={'gradation'} className="min-w-fit">
-    거래완료
+    거래성사
   </Badge>
 )
 export default TradeCompleteBadge
