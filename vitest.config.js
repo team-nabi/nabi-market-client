d7fc7b1167014aba68d4461353d31053e75e71c8
oaoong <pizzachicken@kakao.com>
2023-12-28 12:15:29 +0900

    :sparkles: 테스트 설정 수정

--- vitest.config.js
+++ vitest.config.js
@@ -2,9 +2,6 @@ import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    globals: true,
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
